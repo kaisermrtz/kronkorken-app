7f2dd491dc3f7b3781b82df52ddeed731bedf6fa
Moritz <m.kaiser94@googlemail.com>
2018-04-05 10:05:46 +0200

    Setup first routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 var app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
+hbs.registerPartials(__dirname + '/../views/partials');
+app.set('view engine', 'hbs');
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+//Für Daten die auf mehreren Seiten gleich sind!
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
+app.get('/', (req, res) => {
+  res.render('home.hbs',{
+    pageTitle: 'Kronkorken App',
+    welcomeMessage: 'Willkommen zur NodeJS Kronkorkenseite'
+  });
+});
+
+app.get('/sammlung', (req, res) => {
+  res.render('sammlung.hbs', {
+    pageTitle: 'Kronkorken Sammlung'
+  });
 });
 
 app.listen(port, () => {
