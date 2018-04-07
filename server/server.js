0ac56aa8d815244bb83e475a45c8318e4fff025b
Moritz <m.kaiser94@googlemail.com>
2018-04-07 19:48:06 +0200

    edited styling of the landing page

--- server/server.js
+++ server/server.js
@@ -49,10 +49,26 @@ function isLoggedIn(req){
 }
 
 //GET /
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  var countryCountArrayFirst5;
+  try{
+    var count = await CrownCap.count({});
+    var countryCountArray = await CrownCap.aggregate([{ $group: { _id: '$country', count: { $sum: 1}}}]).sort({count: -1});
+    var countries = countryCountArray.length;
+    countryCountArrayFirst5 = countryCountArray.slice(0,6);
+    console.log(countries);
+  }catch(e){
+    console.log("Error", e);
+  }
+
+
+
   res.render('home.hbs',{
     pageTitle: 'Kronkorken App',
-    loggedIn: isLoggedIn(req)
+    loggedIn: isLoggedIn(req),
+    countryCount: JSON.stringify(countryCountArrayFirst5),
+    count,
+    countries
   });
 });
 
